850d0e43b02a5c489e548a8cd4134042062632c1
Mamed Mamedov <demam777@gmail.com>
2024-01-29 14:56:47 +0300

    main

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-<!doctype html>
-<html lang="en">
-<head>
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+# Для начала определим настройки запуска
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+        Специальный класс, который отвечает за
+        обработку входящих запросов от клиентов
+    """
+
+    @staticmethod
+    def __get_html_content():
+        return '''
+        <!doctype html>
+    <html lang="en">
+    <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
@@ -124,6 +141,8 @@
         .lh-tight {
             line-height: 1.25;
         }
+
+
     </style>
 
 </head>
@@ -285,32 +304,61 @@
                         </div>
                     </div>
                 </div>
-            </div>
-        </main>
-
-        <main class="row">
-            <div class="row text-center">
                 <div class="col-4">
-                    <div class="card mb-4 rounded-3 shadow-sm">
-                        <div class="card-header py-3">
-                            <h4 class="my-0 fw-normal">Товар 1</h4>
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputName1" class="form-label">Имя</label>
+                            <input type="text" class="form-control" id="exampleInputName1">
                         </div>
-                        <div class="card-body">
-                            <h2 class="card-title pricing-card-title">$100</h2>
-                            <p>
-                                10 users included,
-                                2 GB of storage,
-                                Email support,
-                                Help center access
-                            </p>
-                            <button type="button" class="w-100 btn btn-lg btn-success">Купить</button>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1">
+                        </div>
+                        <button type="button" class="btn btn-primary">Отправить</button>
+                    </form>
+                </div>
+            </div>
+
+            <p class="d-flex justify-content-between">
+            <div class="ф text-center">
+                <a class="btn btn-primary" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button"
+                   aria-expanded="false" aria-controls="multiCollapseExample1">Как купить?</a>
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#multiCollapseExample2" aria-expanded="false"
+                        aria-controls="multiCollapseExample2">Как доставить?
+                </button>
+                <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#multiCollapseExample3" aria-expanded="false"
+                        aria-controls="multiCollapseExample3">Какая гарантия?
+                </button>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="collapse multi-collapse" id="multiCollapseExample1">
+                            <div class="card card-body">
+                                Купить можно на нашем сайте.
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="collapse multi-collapse" id="multiCollapseExample2">
+                                <div class="card card-body">
+                                    Доставим быстро.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="collapse multi-collapse" id="multiCollapseExample3">
+                                <div class="card card-body">
+                                    Гарантия год.
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                </p>
         </main>
-
     </div>
+
 
 </main>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
@@ -329,3 +377,31 @@
 </script>
 </body>
 </html>
+        '''
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+
+        page_content = self.__get_html_content()
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.end_headers()  # Завершение формирования заголовков ответа
+        self.wfile.write(bytes(page_content, "utf-8"))  # Тело ответа
+
+
+if __name__ == "__main__":
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
+    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+
+        pass
+
+    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
+    webServer.server_close()
+    print("Server stopped.")
